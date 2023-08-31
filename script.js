44cc9108ad6ab868aebfaf322cc139890f8939d5
kmerediith <kymered24@gmail.com>
2023-08-31 18:43:11 -0400

    added more buttons with different functions

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-alert("test")
+const buttonBc = document.querySelector(".btn-bc")
+const buttonTitle = document.querySelector("#btn-title")
+const buttonCir = document.querySelector("#btn-circles")
+
+console.log({buttonBc, buttonCir, buttonTitle})
+ 
