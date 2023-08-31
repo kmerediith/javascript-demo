48389f4d3a8203f9f37ba460e7f06f6ff9642bf3
kmerediith <kymered24@gmail.com>
2023-08-31 19:40:47 -0400

    updated javascript

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-const buttonBc = document.querySelector(".btn-bc")
-const buttonTitle = document.querySelector("#btn-title")
-const buttonCir = document.querySelector("#btn-circles")
+const buttonBc = document.querySelector(".btn-bc");
+const buttonTitle = document.querySelector("#btn-title");
+const buttonCir = document.querySelector("#btn-circles");
+const body = document.querySelector("body");
+const mainTitle = document.querySelector("#main-title");
 
-console.log({buttonBc, buttonCir, buttonTitle})
- 
+console.log({buttonBc, buttonCir, buttonTitle});
+
+buttonBc.addEventListener("click",()=>{
+    body.style.backgroundColor = "blue";
+})
+
+buttonTitle.addEventListener("click", ()=>{
+    mainTitle.textContent= "I am hungry";
+})
+
+buttonCir.addEventListener("click",()=>{
+    
+})
